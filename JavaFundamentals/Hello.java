431bf14661c05ed406f36ed2917fcb81a1681962
Shiwangi Dubey <dubeyshiwangi79@gmail.com>
2026-04-01 14:26:41 +0530

    Update Hello.java

--- JavaFundamentals/Hello.java
+++ JavaFundamentals/Hello.java
@@ -1,11 +1,23 @@
 package com.basic.java;
 
 public class Hello {
+
+	public void sum() {
+		System.out.println("hiii!");
+	}
+	public void sum2() {
+		System.out.println("hello!!");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-	}
-	public void sum() {
-		System.out.println("hiiii!");
+		Hello obj = new Hello();
+		obj.sum();
+		obj.sum2();
+		System.out.println("java is awesome!");
 	}
 
 }
+
+
+	
